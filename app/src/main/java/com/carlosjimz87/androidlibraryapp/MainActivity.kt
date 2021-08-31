b5c9ad6c475b858071d639be413eaf939362016f
Carlos Jiménez <carlosjimz87@gmail.com>
2021-08-31 19:04:42 +0200

    working lib and app

--- app/src/main/java/com/carlosjimz87/androidlibraryapp/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/androidlibraryapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.carlosjimz87.androidlibraryapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.carlosjimz87.applibrary.Manager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("TRACK_DEBUG", "String ${Manager.getString()}")
+        Log.d("TRACK_DEBUG", "Boolean ${Manager.getBoolean()}")
+        Log.d("TRACK_DEBUG", "Float ${Manager.getFloat()}")
+        Log.d("TRACK_DEBUG", "Int ${Manager.getInt()}")
     }
 }
